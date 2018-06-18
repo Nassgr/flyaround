e92cb9bf6be803bf78c2474505dbb67123e6dd07
Nassgr <anas.grina@hotmail.fr>
2018-06-18 15:24:07 +0200

    test envoi mail ok

--- src/AppBundle/Service/Mailer.php
+++ src/AppBundle/Service/Mailer.php
@@ -8,11 +8,12 @@
 
 namespace AppBundle\Service;
 
-
 class Mailer
 {
     private $mailer;
+
     private $templating;
+
     public function __construct(\Swift_Mailer $mailer, \Twig_Environment $templating)
     {
         $this->mailer = $mailer;
